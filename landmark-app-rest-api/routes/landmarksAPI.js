a6d04ed8af25da6081bf99acebcfffd2073468a1
George Giannopoulos <50545961+GeorgeGiannopoulos@users.noreply.github.com>
2020-07-26 19:31:31 +0300

    [add] #32: REST-API calls to update a photo

--- landmark-app-rest-api/routes/landmarksAPI.js
+++ landmark-app-rest-api/routes/landmarksAPI.js
@@ -1,14 +1,61 @@
 'use strict';
 const express = require('express');
+const multer = require('multer');
+const sharp = require('sharp');
+const crypto = require('crypto');
+
 const router = express.Router();
 module.exports = router;
 
+// =================================================================================================
+// functions
+// =================================================================================================
+const storage = multer.memoryStorage();
+
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png') {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 5242880,
+    },
+    fileFilter: fileFilter,
+});
+
+async function photoPreProcessing(file) {
+    const buffer = file.buffer.toString('base64');
+    const hash = crypto
+        .createHash('md5')
+        .update(buffer)
+        .digest('hex');
+    const original = new Parse.File(hash, { base64: buffer });
+    await original.save();
+
+    const resized = await sharp(file.buffer)
+        .rotate()
+        .resize(250, 250, { fit: 'inside' })
+        .toBuffer();
+    const resized_buffer = resized.toString('base64');
+    const thumbnail = new Parse.File('thumb_' + hash, { base64: resized_buffer });
+    await thumbnail.save();
+
+    return { original, thumbnail };
+}
+
+// =================================================================================================
+// Routes
+// =================================================================================================
 /* landmarks API */
 router.get('/get_landmarks/', async (req, res) => {
     try {
         const Landmark = Parse.Object.extend('Landmarks');
         const query = new Parse.Query(Landmark);
-        query.select('_id', 'title', 'short_info', 'url', 'photo', 'photo_thumb');
+        query.select('_id', 'title', 'short_info', 'photo', 'photo_thumb');
         query.ascending('order');
         const landmarksList = await query.find();
         return res.status(200).json(landmarksList);
@@ -41,6 +88,26 @@ router.post('/update_landmark/:landmark_id', async (req, res) => {
         landmark.set('url', req.body.url);
 
         await landmark.save();
+
+        return res.status(200).json(landmark);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({ ok: false, message: error.message });
+    }
+});
+
+router.post('/update_image/:landmark_id', upload.single('photo'), async (req, res) => {
+    try {
+        const landmark_id = req.params.landmark_id;
+        const Landmark = Parse.Object.extend('Landmarks');
+        const query = new Parse.Query(Landmark);
+        const landmark = await query.get(landmark_id);
+
+        const photo = await photoPreProcessing(req.file);
+        landmark.set('photo', photo.original);
+        landmark.set('photo_thumb', photo.thumbnail);
+
+        await landmark.save(null);
 
         return res.status(200).json(landmark);
     } catch (error) {
